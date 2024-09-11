7cec794aa25e745c1a8f1c12336c7ec258cbe2d8
Elijah Smith <esmith90@huskers.unl.edu>
2024-09-11 13:25:54 -0500

    expand demo

--- src/pages/NarrativeTheology.js
+++ src/pages/NarrativeTheology.js
@@ -1,5 +1,27 @@
 function NarrativeTheology() {
-  return <>This is the Narrative Theology page.</>;
+  return (
+    <>
+      This is the Narrative Theology page.
+      <h1>Narrative Theology</h1>
+      <p>
+        Narrative theology is a way of understanding the Bible as a story. This
+        story is not just a story of the past, but a story that continues to
+        unfold in the present. It is a story that invites us to participate in
+        the ongoing work of God in the world.
+      </p>
+      <div>
+        <a href="/narrative-theology">Definition of Narrative Theology</a>
+      </div>
+      <div>
+        <a href="/narrative-theology">Problem of Divine Hiddeness</a>
+      </div>
+      <div>
+        <a href="/narrative-theology">
+          Scrutiny against the Philosophical Standard
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default NarrativeTheology;
